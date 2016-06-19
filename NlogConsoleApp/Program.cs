eae1ebd69706f4aeae2fde75c6598e5929868746
ss <kalsiss@gmail.com>
2016-06-19 21:27:59 +1000

    Change Readme.md and add solution

---
+++ NlogConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NlogConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int zero = 0;
+                int result = 5 / zero;
+            }
+            catch (DivideByZeroException ex)
+            {
+                // get a Logger object and log exception here using NLog
+                // this will use our default logger, because we named it '*' in the NLog.config file
+                Logger logger = LogManager.GetCurrentClassLogger();
+                // add custom message and pass in the exception
+              //  logger.Error("Whoops!", ex);
+                logger.Error(ex, "Whoops");
+            }
+        }
+    }
+}
--- Readme.md
+++ Readme.md
@@ -1,1 +1,5 @@
-#NLogConse
+#NLogConsole
+
+A simple .Net Console Application that uses Nlog package
+and logs error to file
+Using VS studio 2015 community edition
